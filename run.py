4dd75a9b9820c0ddb4fa0ca75ca9ec05cbf59191
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-10-26 14:53:34 -0200

    Simple map-reduce

--- run.py
+++ run.py
@@ -1,8 +1,37 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
+import random
+
+from multiprocessing import Pool
+import time
+
+def square_map(x):
+    return x * x
+
 def main():
-    pass
+    p = Pool(5)
+
+    i = int(random.random() * 10)
+    j = int(random.random() * 10)
+
+    if i > j:
+        i, j = j, i
+    elif i == j:
+        j = int(random.random() + 1) + i
+
+    l = list(range(i, j))
+
+    result = p.map_async(square_map, l)
+
+    map_result = result.get()
+
+    reduce_result = reduce(lambda x, y: x + y, map_result)
+
+    print 'The sum of the squares of numbers in {} is: {}'.format(l, reduce_result)
+
+    p.close()
+    p.join()
 
 if __name__ == '__main__':
     main()
